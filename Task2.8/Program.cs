64b66c0838c2a3a7c555f4285213225adaef9703
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-04 21:28:04 +0200

    Добавьте файлы проекта.

---
+++ Task2.8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть довжину сторони a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть довжину сторони b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть довжину сторони c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        if (a > b && a > c)
+        {
+            Console.WriteLine("Сторона a є найдовшою.");
+        }
+        else if (b > a && b > c)
+        {
+            Console.WriteLine("Сторона b є найдовшою.");
+        }
+        else if (c > a && c > b)
+        {
+            Console.WriteLine("Сторона c є найдовшою.");
+        }
+        else
+        {
+            Console.WriteLine("Дві або більше сторін мають однакову довжину та є найдовшими.");
+        }
+    }
+}
